1098f6245ded7dbbe203f1b6d840bf31a3beb51a
Sangameshwar Mandakanalli <smandaka@progress.com>
2021-10-26 15:52:36 +0530

    Version bump from 563 to 590
    Signed-off-by: Sangameshwar Mandakanalli <smandaka@progress.com>

--- plan.sh
+++ plan.sh
@@ -1,6 +1,6 @@
 pkg_name=less
 pkg_origin=core
-pkg_version=563
+pkg_version=590
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="\
 A terminal pager program used to view (but not change) the contents of a text \
@@ -9,7 +9,7 @@ file.\
 pkg_upstream_url="http://www.greenwoodsoftware.com/less/index.html"
 pkg_license=('gplv3+')
 pkg_source="http://www.greenwoodsoftware.com/$pkg_name/${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum="ce5b6d2b9fc4442d7a07c93ab128d2dff2ce09a1d4f2d055b95cf28dd0dc9a9a"
+pkg_shasum="6aadf54be8bf57d0e2999a3c5d67b1de63808bb90deb8f77b028eafae3a08e10"
 pkg_deps=(
   core/glibc
   core/ncurses
